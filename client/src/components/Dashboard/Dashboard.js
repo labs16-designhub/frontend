31ab9c8e4bbd77909d75272522cb195d3cf4701c
Ian Be <ian84be@gmail.com>
2019-09-11 18:23:57 -0500

    scaffold Dashboard component

---
+++ client/src/components/Dashboard/Dashboard.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Dashboard.scss';
+
+const tempData = {
+  firstName: 'Erik',
+  lastName: 'Lambert',
+  username: 'eriklambert',
+  bio:
+    'I love designing and always looking for ways to improve and innovate. 🤓 #Usersmatter! #LambdaBound #UXEngineer',
+  location: 'Austin, TX',
+  website: 'https://eriklambert.io',
+  userProjectsNum: '12',
+  userFollowersNum: '36',
+  userFollowingNum: '1',
+  starredProjectsNum: '143'
+};
+
+const Dashboard = props => {
+  return <div className="Dashboard">Dashboard</div>;
+};
+
+export default Dashboard;
