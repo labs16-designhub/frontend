fc5074340de58bda7aeeacf2d922667ed9f3c842
Ian Be <ian84be@gmail.com>
2019-09-11 19:04:22 -0500

    buildout miniProfile section

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import './App.css';
 import UserProfile_LI from './components/UserProfile_LI';
+import Dashboard from './components/Dashboard/Dashboard';
 
 function App() {
   return (
     <div className="App">
       <Route exact path="/profile" component={UserProfile_LI} />
+      <Route exact path="/dashboard" component={Dashboard} />
     </div>
   );
 }
--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './Dashboard.scss';
 
+const fakeAvatar = require('./fakeAvatar.jpg');
+const locationIcon = require('./location.svg');
+const linkIcon = require('./link.svg');
+
 const tempData = {
   firstName: 'Erik',
   lastName: 'Lambert',
   username: 'eriklambert',
+  avatar: fakeAvatar,
   bio:
     'I love designing and always looking for ways to improve and innovate. 🤓 #Usersmatter! #LambdaBound #UXEngineer',
   location: 'Austin, TX',
@@ -16,7 +21,46 @@ const tempData = {
 };
 
 const Dashboard = props => {
-  return <div className="Dashboard">Dashboard</div>;
+  const {
+    firstName,
+    lastName,
+    username,
+    bio,
+    avatar,
+    location,
+    website
+  } = tempData;
+  return (
+    <div className="Dashboard">
+      <div className="miniProfile">
+        <img src={avatar} alt="userPhoto" />
+
+        <div className="profileDetails">
+          <h3>
+            {firstName}&nbsp;{lastName}
+          </h3>
+          <h4>{username}</h4>
+          <h5>{bio}</h5>
+
+          <div className="bottomRow">
+            <div className="dataPoint">
+              <img src={locationIcon} alt="locationIcon" />
+              {location}
+            </div>
+
+            <div className="dataPoint">
+              <img src={linkIcon} alt="linkIcon" />
+              <a href={website} target="_blank">
+                {website}
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="miniStats"></div>
+    </div>
+  );
 };
 
 export default Dashboard;
